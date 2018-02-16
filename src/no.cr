22c5023dd0b51e57543be962641ed4bf08717da2
Jens <jens0512@osloskolen.no>
2018-02-16 22:24:03 +0100

    Yay

--- src/no.cr
+++ src/no.cr
@@ -5,11 +5,11 @@ q? = false
 
 OptionParser.parse! do |parser|
   parser.banner = "Usage: No [opts] [args]"
-  parser.on("-?", "--or", "Or what?") { q? = true }
+  parser.on("-o", "--or", "Or what?") { q? = true }
   parser.on("-h", "--help", "Show this help") { puts parser }
 
   parser.unknown_args do |args|
-    puts %("#{args.join(" ")}" No#{q? ? "?" : ""})
+    puts %("#{args.empty? "Heh" : (args.join(" "))}" No#{q? ? "?" : "."})
   end
 end
 
