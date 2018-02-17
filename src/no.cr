381e949e4b3a8c64f31d1e030b52442add363dd8
Jens <jens0512@osloskolen.no>
2018-02-17 16:51:35 +0100

    -h now exits after the help is printed

--- src/no.cr
+++ src/no.cr
@@ -6,10 +6,10 @@ q? = false
 OptionParser.parse! do |parser|
   parser.banner = "Usage: No [opts] [args...]"
   parser.on("-o", "--or", "Or what?") { q? = true }
-  parser.on("-h", "--help", "Show this help") { puts parser }
+  parser.on("-h", "--help", "Show this help") { puts parser; exit }
 
   parser.unknown_args do |args|
-    puts %<"#{args.empty? ? "<nothing>" : (args.join(" "))}" No#{q? ? "?" : "."}>
+    puts %["#{args.empty? ? "<nothing>" : (args.join(" "))}" No#{q? ? "?" : "."}]
   end
 end
 
