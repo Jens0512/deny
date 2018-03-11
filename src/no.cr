84211df36d5a0cd0025d3a2d4c8aecebb4a5abe5
Jens <jens0512@osloskolen.no>
2018-03-11 19:05:47 +0100

    Added -s and -d options, see --help for more details

--- src/no.cr
+++ src/no.cr
@@ -2,14 +2,19 @@ require "./no/*"
 require "option_parser"
 
 q? = false
+surr = '"'
+denial = "No"
 
 OptionParser.parse! do |parser|
   parser.banner = "Usage: No [opts] [args...]"
-  parser.on("-o", "--or", "Or what?") { q? = true }
+
+  parser.on("-o", "--or", "Or what? Appends '?' to the output") { q? = true }
+  parser.on("-s SURROUND", "--surround=SURROUND", "Sets what to surround the args with (default: #{surr})") { |in|; surr = in }
+  parser.on("-d DENIAL", "--delimeter=DENIAL", "Sets how the joined args are denied (default: #{denial})") { |in|; denial = in }
   parser.on("-h", "--help", "Show this help") { puts parser; exit }
 
   parser.unknown_args do |args|
-    puts %["#{args.empty? ? "<nothing>" : (args.join(" "))}" No#{q? ? "?" : "."}]
+    puts %[#{surr}#{args.empty? ? %(<nothing>) : (args.join(' '))}#{surr} #{denial}#{q? ? '?' : '.'}]
   end
 end
 
